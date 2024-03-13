117dbc8794b7fb68872e5ea14718f44f4a534f7a
MrAmirRezaie <mramirrezaie80@gmail.com>
2024-03-13 01:12:46 -0700

    upadet-6

--- test1/Cpplearning-Part[A].cpp
+++ test1/Cpplearning-Part[A].cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//Part[A]
+
+//Write a program that takes two numbers from the user and prints the positive numbers between the two numbers.
+
 int main()
 {
-	//Part[A]
 
-	/*
-	//Write a program that takes two numbers from the user and prints the positive numbers between the two numbers.
 	double num1,
 		num2;
 
@@ -23,50 +24,6 @@ int main()
 			cout << i << "	";
 		}
 	}
-	*/
 
-	//Part[B]
-
-	/*
-	//Write a program that prints the numbers between zero and hundred
-	
-	for (int i = 0; i <= 100; i++)
-	{
-		cout << i << " ";
-	}
-	*/
-
-	//Part[C]
-
-	/*
-	//Write a program that receives two numbers from the user and then calculates their sum and displays them in the output.
-
-	double num1,
-		num2;
-
-	cout << "Enter your first number : ";
-	cin >> num1;
-
-	cout << "Enter your second number : ";
-	cin >> num2;
-
-	cout << "The sum of the two numbers entered by you is equal to : " << num1 + num2 << endl;
-	*/
-
-	//Part[D]
-
-	/*
-	//Write a program that displays the ASCII code of the character.
-
-	char x;
-
-	cout << "Enter your character : ";
-	cin >> x;
-
-	short int y;
-	y = x;
-
-	cout << "The ASCII character code entered by you is equal to : " << y << endl;
-	*/
 	return 0;
 }
